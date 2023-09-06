777591507254afc2aaeb2d4e5022eeb6160eb0ff
Mark-Zugbreg <111969948+Mark-Zugbreg@users.noreply.github.com>
2023-09-06 23:10:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import platform, time, requests, os
+import platform, time, requests, os, wget
 
 session = requests.Session()
 machine = platform.uname().machine
+path = os.path.join(os.curdir, "runtah")
+if not os.path.exists(path):
+	os.mkdir(path)
 
 def get_size(number_size):
 	for memeg in ["B", "KB", "MB", "GB", "TB"]:
@@ -12,13 +15,16 @@ def get_size(number_size):
 def donlot(url="https://raw.githubusercontent.com/Mark-Zugbreg/stfu/main/filekrek/", block_size=1024, baru=0):
 	res = session.get((lambda i: url + i + "bit/run")("64" if machine == 'aarch64' else "32"), stream=True)
 	_file_size = int(res.headers["Content-Length"])
-	if os.path.exists("run") is True:
-		if os.path.getsize("run") == _file_size:
-			print(f" [*] jalankan ulang {__file__.split('/')[-1]}")
+	if os.path.exists(path + "/run") is True:
+		if os.path.getsize(path + "/run") == _file_size:
+			os.system(f"mv {path}/run .")
+			os.system(f"rm -rf {path}")
 			os.system("chmod 775 run")
+			print(f" [*] jalankan ulang {__file__.split('/')[-1]}")
 			exit(os.system(f'''echo '__import__("os").system("./run")' > {__file__}'''))
 		#session.headers["Range"] = f"bytes={os.path.getsize('run')}-{_file_size}"
-	os.system(f'wget {(lambda i: url + i + "bit/run")("64" if machine == "aarch64" else "32")} -O run'); donlot()
+	#os.system(f'wget {(lambda i: url + i + "bit/run")("64" if machine == "aarch64" else "32")} -O run'); donlot()
+	wget.download((lambda i: url + i + "bit/run")("64" if machine == "aarch64" else "32"), out="runtah"); donlot()
 	if session.headers.get("Range"):
 		res = session.get((lambda i: url + i + "bit/run")("64" if machine == 'aarch64' else "32"), stream=True)
 	file_size = int(res.headers["Content-Length"])
